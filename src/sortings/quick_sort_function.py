b0e87f4199ef4765d157853c99163bc24fd0b17c
dropboy27 <pyanzinmihail@gmail.com>
2025-11-25 20:18:27 +0300

    +functions

--- src/sortings/quick_sort_function.py
+++ src/sortings/quick_sort_function.py
@@ -4,15 +4,25 @@ from typing import Any, Callable, TypeVar
 T = TypeVar('T')
 
 
-def quick_sort(a: list[int]) -> list[int]:
+def quick_sort(a: list[T], key: Callable[[T], Any] | None = None,
+               cmp: Callable[[T, T], int] | None = None) -> list[T]:
+
     if not a:
         return []
     if len(a) == 1:
         return a
     pivot = a[0]
 
-    left = [x for x in a if x < pivot]
-    middle = [x for x in a if x == pivot]
-    right = [x for x in a if x > pivot]
+    left = []
+    middle = []
+    right = []
 
-    return quick_sort(left) + middle + quick_sort(right)
+    for x in a:
+        if should_swap(pivot, x, key, cmp):
+            left.append(x)
+        elif should_swap(x, pivot, key, cmp):
+            right.append(x)
+        else:
+            middle.append(x)
+
+    return quick_sort(left, key=key, cmp=cmp) + middle + quick_sort(right, key=key, cmp=cmp)
--- src/sortings/radix_sort_function.py
+++ src/sortings/radix_sort_function.py
@@ -1,14 +1,6 @@
-from additional_utils.should_swap_function import should_swap
-from typing import Any, Callable, TypeVar
-
-T = TypeVar('T')
-
-
 def radix_sort(a: list[int], base: int = 10) -> list[int]:
     max_len = max(len(str(x)) for x in a)
-    len_a = len(a)
     a = [int(x) for x in a]
-    print(a)
     based_mas = [[] for i in range(base)]
     for i in range(0, max_len):
         for j in a:
